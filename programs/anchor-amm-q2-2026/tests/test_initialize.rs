91c209ed2463c5956328741911e3d84b059b18fb
Ashutosh Sagar <ashu.sagar111@gmail.com>
2026-05-22 19:07:35 +0000

    added test setup

--- programs/anchor-amm-q2-2026/tests/test_initialize.rs
+++ programs/anchor-amm-q2-2026/tests/test_initialize.rs
@@ -8,6 +8,63 @@ use {
     solana_transaction::versioned::VersionedTransaction,
 };
 
+fn send(
+    svm: &mut LiteSVM,
+    ixs: &[Instruction],
+    payer: &Keypair,
+    signers: &[&Keypair],
+) -> litesvm::types::TransactionResult {
+    svm.expire_blockhash();;
+    let blockhash = svm.latest_blockhash();
+    let msg = Message::new_with_blockhash(ixs, Some(&payer.pubkey()), &blockhash);
+    let tx = VersionedTransaction::try_new(VersionedMessage::Legacy(msg), signers).unwrap();
+    svm.send_transaction(tx)
+}
+
+fn setup() -> (
+    LiteSVM,
+    Keypair,
+    Pubkey,
+    Pubkey,
+    Pubkey,
+    Pubkey,
+    Pubkey,
+    Pubkey,
+) {
+    let program_id = amm_video::id();
+    let payer = Keypair::new();
+    let mut svm = LiteSVM::new();
+    let bytes = include_bytes!("../../../target/deploy/amm_video.so");
+    svm.add_program(program_id, bytes).unwrap();
+    svm.airdrop(&payer.pubkey(), 1_000_000_000).unwrap();
+
+    // Create two mints (Mint A and Mint B) with 6 decimal places and the maker as the authority
+    // This done using litesvm-token's CreateMint utility which creates the mint in the LiteSVM environment
+    let mint_x = CreateMint::new(&mut svm, &payer)
+        .decimals(6)
+        .authority(&payer.pubkey())
+        .send()
+        .unwrap();
+
+    let mint_y = CreateMint::new(&mut svm, &payer)
+        .decimals(6)
+        .authority(&payer.pubkey())
+        .send()
+        .unwrap();
+
+    let config =
+        Pubkey::find_program_address(&[b"config", &123u64.to_le_bytes()], &amm_video::id()).0;
+    let mint_lp = Pubkey::find_program_address(&[b"lp", config.as_ref()], &amm_video::id()).0;
+
+    // Derive the PDA for the vault associated token account using the config PDA and Mint A
+    let vault_x = associated_token::get_associated_token_address(&config, &mint_x);
+    let vault_y = associated_token::get_associated_token_address(&config, &mint_y);
+
+    (
+        svm, payer, mint_x, mint_y, config, mint_lp, vault_x, vault_y,
+    )
+}
+
 #[test]
 fn test_initialize() {
     let program_id = anchor_amm_q2_2026::id();
